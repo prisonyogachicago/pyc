57905b31f53096c2c702243b5fffc7771c56feb6
Gary Cuga-Moylan <gecugamo@gmail.com>
2021-02-21 16:02:16 -0600

    Navbar

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@
 			}
 		});
 
-		if ($(window).width() > 766) {
+		if (window.innerWidth > 767) {
 			$('body').removeClass('probootstrap-mobile-menu-active');
 			$('.probootstrap-burger-menu').removeClass('active');
 		} else {
@@ -85,7 +85,7 @@
 		}
 
 		$(window).resize(function(){
-			if ($(window).width() > 766) {
+			if (window.innerWidth > 767) {
 				$('body').removeClass('probootstrap-mobile-menu-active');
 				$('.probootstrap-burger-menu').removeClass('active');
 			} else {
